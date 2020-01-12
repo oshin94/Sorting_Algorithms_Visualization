bd04ffe1cab724563fd5c4e09856f6a3af5c1990
Arnab Paul Choudhury <arnabpc.oshin94@gmail.com>
2020-01-12 12:30:31 +0530

    added insertion sort and randomization button

--- static/render.js
+++ static/render.js
@@ -32,10 +32,13 @@ function bubble_sort(){
 	
 	runLoop = async () => {
 		k=0
+		m=0
 		while(swapd){
 			swapd=false;
 			console.log("\n\nIteration: ", k);
+			m++
 			for(var j=0; j<num_of_bar; j++){
+				m++
 				await new Promise( resolve => setTimeout( resolve, 1 ) )
 				if ((j+1)<num_of_bar && height[j]>height[j+1]){
 					ele = document.getElementById(j);
@@ -67,59 +70,98 @@ function bubble_sort(){
 				else{
 					console.log("no change");
 				}
-			}
 			k++
+			}
 		}
-	console.log("Sorting done");
+	console.log("Sorting done in ",m," iterations");
 	var l = document.getElementById("info");
-	l.innerHTML = "The bars have been bubble sorted in an ascending manner..!\n HURRAY...!!!"
+	l.innerHTML = "The bars have been bubble sorted in an ascending manner..!\n HURRAY...!!!\n Iterations: "+m;
 	}
 	runLoop()
 }
 
 
-/* function bubble_sort(){
-	//swapd=true;
-	
+function insertion_sort(){
+
 	runLoop = async () => {
-		for(var k=0; k<50; k++){
-			console.log("\n\nIteration: ", k);
-			for(var j=0; j<50; j++){
+		k=0
+		//for i in range(1, len(x)-1):
+		for (var i=0; i<num_of_bar; i++){
+			k++;
+			console.log("i is: ",i)
+			//for j in range(i,0,-1):
+			for (var j=i;j>0;j--){
+				k++
+				console.log("j is: ", j)
 				await new Promise( resolve => setTimeout( resolve, 1 ) )
-				if ((j+1)<50 && height[j]>height[j+1]){
+				if (height[j]<height[j-1]){
 					ele = document.getElementById(j);
 					console.log("\nelement before change: ",ele);
-					next_ele = document.getElementById(j+1);
-					console.log("Next element before change: ",next_ele);
+					prev_ele = document.getElementById(j-1);
+					console.log("Next element before change: ",prev_ele);
 					
-					temp = height[j+1];
-					temp_svg = next_ele.getAttribute('x');
-					temp_id = next_ele.getAttribute('id')
 					
-					height[j+1] = height[j];
-					next_ele.setAttribute("x", ele.getAttribute("x"));
-					next_ele.setAttribute("id", ele.getAttribute("id"));
+					temp = height[j]
+					temp_svg = ele.getAttribute('x');
+					temp_id = ele.getAttribute('id')
 					
-					height[j] = temp;
-					ele.setAttribute("x", temp_svg);
-					ele.setAttribute("id", temp_id);
+					height[j] = height[j-1]
+					ele.setAttribute("x", prev_ele.getAttribute("x"));
+					ele.setAttribute("id", prev_ele.getAttribute("id"));
+					
+					height[j-1] = temp
+					prev_ele.setAttribute("x", temp_svg);
+					prev_ele.setAttribute("id", temp_id);
+					
 					
 					ele = '';
-					next_ele = '';
+					prev_ele = '';
 					ele = document.getElementById(j);
 					console.log("\nelement after change: ",ele);
-					next_ele = document.getElementById(j+1);
-					console.log("Next element after change: ",next_ele);
-					console.log("Height is: ", height);
+					next_ele = document.getElementById(j-1);
+					console.log("Previous element after change: ",next_ele);
+					console.log(height)
 				}
 				else{
-					console.log("no change");
+					break
 				}
 			}
 		}
+	console.log("Sorted array is: ", height)
+	console.log("Total iterations: ", k);
+	
+	var l = document.getElementById("info");
+	l.innerHTML = "The bars have been Insertion Sorted in an ascending manner..!\n HURRAY...!!!\n Iterations: "+k;
+	
 	}
-	runLoop()
-} */
+	runLoop();
+}
+
+function randomize(){
+svg = document.getElementsByTagName('svg')[0]; //Get svg element
+	
+	//id=0;
+	height=[];
+	num_of_bar = 50
+	width_of_bar = 10
+	act_width_of_bar = width_of_bar-2
+	for (var i=0;i<50;i++){
+		h = Math.floor(Math.random() * 200) + 1
+		y = 200-h;
+		height.push(h);
+		ele = document.getElementById(i);
+		console.log("\nelement before change: ",ele);
+		//var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'rect'); //Create a path in SVG's namespace
+		//newElement.setAttribute("width",act_width_of_bar);
+		ele.setAttribute("height",h);
+		//newElement.setAttribute("x",i);
+		ele.setAttribute("y",y);
+		//newElement.setAttribute("style","fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)");
+		//newElement.setAttribute("id",id);
+		//svg.appendChild(newElement);
+		//id++;
+	}
+}
 	
 
 
